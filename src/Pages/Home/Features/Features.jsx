e9f2a096ed7352ae2a7029ac9daed6dbb84df379
seyam <mdseyam3215@gmail.com>
2023-11-26 01:16:42 +0600

    features section mobile responsive

--- src/Pages/Home/Features/Features.jsx
+++ src/Pages/Home/Features/Features.jsx
@@ -1,27 +1,26 @@
-import {  useLoaderData } from "react-router-dom";
+
 import FeatureCard from "../FeatureCard/FeatureCard";
 import SectionTitle from "../../../Component/SectionTitle/SectionTitle";
-
+import { useLoaderData } from "react-router-dom";
 
 const Features = () => {
-    const features =useLoaderData();
-     console.log(features);
+  const features = useLoaderData();
 
-    return (
-      <div>
-          <SectionTitle
-            subHeading="Discover Our Featured Workouts and Nutrition Insights for a Balanced Lifestyle"
-            heading={'Unlock Your Peak Performance'}
-        ></SectionTitle>
-        <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-5 mx-auto p-5 mt-20">
-          {features.map((feature) => (
-            <div key={feature.id}>
-                <FeatureCard  feature={feature} key={feature.id}></FeatureCard>
-            </div>
-          ))}
-        </div>
-        </div>
-    );
+  return (
+    <div className="container mx-auto mt-20 px-4 py-10  md:px-8 lg:px-16 ">
+      <SectionTitle
+        subHeading="Discover Our Featured Workouts and Nutrition Insights for a Balanced Lifestyle"
+        heading="Unlock Your Peak Performance"
+      ></SectionTitle>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {features.map((feature) => (
+          <div key={feature.id} className="mb-4">
+            <FeatureCard feature={feature}></FeatureCard>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Features;
