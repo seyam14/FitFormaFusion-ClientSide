12be09794e70c7370c72fdcf84f1f0585314589c
seyam <mdseyam3215@gmail.com>
2023-11-28 01:44:40 +0600

    apliedtrainer updated

--- src/Pages/Dashboard/ApliedTrainer/ApliedTrainerTable/ApliedTrainerTable.jsx
+++ src/Pages/Dashboard/ApliedTrainer/ApliedTrainerTable/ApliedTrainerTable.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+
 
 const ApliedTrainerTable = ({item}) => {
     console.log(item);
@@ -10,9 +10,7 @@ const ApliedTrainerTable = ({item}) => {
           <td>{fullName}</td>
           <td>{email}</td>
            <td> {age}</td>
-           <Link to='/'>
            <button className="btn btn-secondary m-2"> Action </button>
-           </Link>
         </tr>
      
       );
