f31683b5f4bdaa6ce034b742965409a9f3302478
seyam <mdseyam3215@gmail.com>
2023-11-29 02:10:02 +0600

    update

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,24 +1,12 @@
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import CheckOutForm from './CheckOutForm';
-// import { useEffect, useState } from 'react';
-// import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 
 const stripePromise  = loadStripe(import.meta.env.VITE_Payment_Gateway_PK)
 const Payment = () => {
     const {salary, _id} = useParams();
-  
-    // const [trainer, setTrainer] = useState([]);
-    //   console.log(trainer);
-  
-    // useEffect(() => {
-    //   axios.get('http://localhost:5050/trainer/${salary}')
-    //     .then(res => res.data)
-    //     .then(data => setTrainer(data))
-    //     .catch(error => console.error('Error fetching data:', error));
-    // }, []);
   
     return (
       <div>
