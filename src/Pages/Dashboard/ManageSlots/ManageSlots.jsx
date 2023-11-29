a39a3f4384c042975c9a78721dc92d1e3e41ed84
seyam <mdseyam3215@gmail.com>
2023-11-29 22:42:38 +0600

    use Material-UI

--- src/Pages/Dashboard/ManageSlots/ManageSlots.jsx
+++ src/Pages/Dashboard/ManageSlots/ManageSlots.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import emailjs from 'emailjs-com';
+import Swal from 'sweetalert2';
+import { Button, Grid, Paper, Typography } from '@mui/material'; // Import Material-UI components
+
 import SectionTitle from "../../../Component/SectionTitle/SectionTitle";
 
 const ManageSlots = () => {
-
- const [slots, setSlots] = useState([
+  const [slots, setSlots] = useState([
     { id: 1, time: '9:00 AM', status: 'available' },
     { id: 2, time: '10:00 AM', status: 'booked', booked_by: 'User' },
     { id: 3, time: '5:00 PM', status: 'available' },
@@ -12,54 +14,65 @@ const ManageSlots = () => {
   ]);
 
   const handleRejectBooking = (slotId, bookedBy) => {
-
     const updatedSlots = slots.map((slot) =>
       slot.id === slotId ? { ...slot, status: 'available' } : slot
     );
 
     sendRejectionEmail(bookedBy);
+    showSuccessToast();
 
     setSlots(updatedSlots);
   };
 
   const sendRejectionEmail = (userEmail) => {
- 
     emailjs.send('service_khj8eai', 'template_o4zwkcq', { to_email: userEmail, message: 'Your booking has been rejected.' }, 'FOkhzyqNBrWXCKfZ7');
+  };
+
+  const showSuccessToast = () => {
+    Swal.fire({
+      icon: 'success',
+      title: 'Email Sent',
+      text: 'Rejection email has been sent successfully!',
+    });
   };
 
   return (
     <div className="container mx-auto mt-8">
       <SectionTitle
-      subHeading="trainer choose slot "
-      heading=" Slots"
-    ></SectionTitle>
-      <div className="grid grid-cols-2 gap-4">
+        subHeading="trainer choose slot "
+        heading=" Slots"
+      ></SectionTitle>
+      <Grid container spacing={2}>
         {slots.map((slot) => (
-          <div
-            key={slot.id}
-            className={`p-4 border ${
-              slot.status === 'booked' ? 'bg-red-200' : 'bg-green-200'
-            }`}
-          >
-            <p className="text-lg font-bold mb-2">Time: {slot.time}</p>
-            <p>Status: {slot.status}</p>
-            {slot.status === 'booked' && (
-              <p>Booked by: {slot.booked_by}</p>
-            )}
-            {slot.status === 'booked' && (
-              <button
-                onClick={() => handleRejectBooking(slot.id, slot.booked_by)}
-                className="mt-2 bg-red-500 text-white px-4 py-2 rounded"
-              >
-                Reject Booking
-              </button>
-            )}
-          </div>
+          <Grid item key={slot.id} xs={6} md={3}>
+            <Paper
+              className={`p-4 ${
+                slot.status === 'booked' ? 'bg-red-200' : 'bg-green-200'
+              }`}
+            >
+              <Typography variant="h6" gutterBottom>
+                Time: {slot.time}
+              </Typography>
+              <Typography variant="body1">Status: {slot.status}</Typography>
+              {slot.status === 'booked' && (
+                <Typography variant="body1">Booked by: {slot.booked_by}</Typography>
+              )}
+              {slot.status === 'booked' && (
+                <Button
+                  onClick={() => handleRejectBooking(slot.id, slot.booked_by)}
+                  variant="contained"
+                  color="secondary"
+                  className="mt-2"
+                >
+                  Reject Booking
+                </Button>
+              )}
+            </Paper>
+          </Grid>
         ))}
-      </div>
+      </Grid>
     </div>
   );
 };
-
 
 export default ManageSlots;
