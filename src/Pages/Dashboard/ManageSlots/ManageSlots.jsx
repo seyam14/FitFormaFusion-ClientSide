5a135e283b3da312e46a6f2446eb2b672eb30eb5
seyam <mdseyam3215@gmail.com>
2023-11-29 19:11:13 +0600

    trainer part slot again

--- src/Pages/Dashboard/ManageSlots/ManageSlots.jsx
+++ src/Pages/Dashboard/ManageSlots/ManageSlots.jsx
@@ -3,17 +3,16 @@ import emailjs from 'emailjs-com';
 import SectionTitle from "../../../Component/SectionTitle/SectionTitle";
 
 const ManageSlots = () => {
- // Sample slots state
+
  const [slots, setSlots] = useState([
     { id: 1, time: '9:00 AM', status: 'available' },
     { id: 2, time: '10:00 AM', status: 'booked', booked_by: 'User' },
     { id: 3, time: '5:00 PM', status: 'available' },
     { id: 4, time: '7:00 PM', status: 'booked', booked_by: 'User' },
-    // ... other slots
   ]);
 
   const handleRejectBooking = (slotId, bookedBy) => {
-    // Logic to reject booking
+
     const updatedSlots = slots.map((slot) =>
       slot.id === slotId ? { ...slot, status: 'available' } : slot
     );
@@ -24,9 +23,7 @@ const ManageSlots = () => {
   };
 
   const sendRejectionEmail = (userEmail) => {
-    // Use emailJS to send rejection email
-    // Include relevant information in the email template
-    // Ensure you have configured emailJS with the correct service ID, template ID, and user ID
+ 
     emailjs.send('service_khj8eai', 'template_o4zwkcq', { to_email: userEmail, message: 'Your booking has been rejected.' }, 'FOkhzyqNBrWXCKfZ7');
   };
 
